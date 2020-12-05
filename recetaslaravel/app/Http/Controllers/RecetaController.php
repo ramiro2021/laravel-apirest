dd9cf53db81fa045e17eafc9d691f2f844d9a05f
ramiro2021 <ramiroamurua@hotmail.com>
2020-12-05 17:44:21 -0300

    feat : inicio recetas laravel-vue

---
+++ recetaslaravel/app/Http/Controllers/RecetaController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RecetaController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+
+        $recetas = ['Receta Pizza', 'Receta Hamburguesa', 'Receta Tacos'];
+
+        // return view('recetas.index')->with('recetas', $recetas);
+
+
+        return view('recetas.index', compact('recetas'));
+    }
+}
---
+++ recetaslaravel/resources/views/recetas/index.blade.php
@@ -0,0 +1,6 @@
+<h1>Recetas</h1>
+
+
+@foreach($recetas as $receta)
+    <li>{{$receta}}</li>
+@endforeach
